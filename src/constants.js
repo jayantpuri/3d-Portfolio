e567e7d58e69af54b90d3e21fa7fd5358bbd1cb7
jayant puri <jayantpuri16@gmail.com>
2024-09-17 19:24:42 -0700

    added the desk 3D model

--- src/constants.js
+++ src/constants.js
@@ -20,3 +20,14 @@ export const navLinks = [
     href: "#contact",
   },
 ];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,26 @@
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { Suspense } from "react";
+
+import { PerspectiveCamera } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
+
+import HackerRoom from "../componets/HackerRoom";
+import Loader from "../componets/Loader";
+import { calculateSizes } from "../constants";
 
 const Hero = () => {
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTbalet = useMediaQuery({ maxWWWidth: 1024, minWidth: 768 });
+
+  const { deskPosition, deskScale } = calculateSizes(
+    isSmall,
+    isMobile,
+    isTbalet
+  );
+
   return (
-    <div className="min-h-screen w-full mx-auto flex flex-col items center">
+    <div className="min-h-screen w-full mx-auto flex flex-col relative">
       <div className="w-full flex flex-col text-center c-space mx-auto gap-3 sm:mt-36 mt-24">
         <p className="sm:text-3xl text-1xl text-gray-300">
           Hi, my name is Jayant{" "}
@@ -16,8 +33,19 @@ const Hero = () => {
           Building Products & Brands
         </p>
       </div>
-      <div>
-        <Canvas></Canvas>
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="h-full w-full">
+          <Suspense fallback={<Loader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+            <HackerRoom
+              position={[1.2, -7, 1.9]}
+              rotation={[0.3, -3.1, 0]}
+              scale={0.1}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </div>
   );
